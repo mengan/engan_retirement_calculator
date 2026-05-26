d234972140263e7470c8e8a2086aedd485d4b01d
Mike engan <mike.engan@gmail.com>
2026-05-26 12:56:58 -0700

    auto-sync: update app.js

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ const defaultState = () => ({
       loanBalance: 120000, payment: 1400, escrow: 350, interestRate: 5.0,
       isRental: true, rent: 2400, rentGrowth: 3, basis: 220000, sellYear: 2040, yearsDepreciated: 10, taxablePct: 30 },
   ],
+  futurePurchases: [],
 });
 
 let state;
