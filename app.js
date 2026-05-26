31e8f7ffcf348d940ebe5bc589869793bc78a9bf
Mike engan <mike.engan@gmail.com>
2026-05-26 14:01:28 -0700

    auto-sync: update app.js

--- app.js
+++ app.js
@@ -1657,7 +1657,7 @@ function project(opts) {
           .filter(a => a.type === "inherited_ira")
           .reduce((sum, a) => sum + a.balance, 0);
         const suppressRoth = (!!rc.startAfterInheritedDepleted && inhBalanceAfterDrain > 0)
-          || (!!rc.skipConversionOnSaleYear && properties.some(p => p.sellYear === year && p.sellYear > 0));
+          || suppressSaleYear;
 
         if (inFillWindow && !suppressRoth && headroom > 0) {
           rothConverted = performRothConversion(accounts, headroom);
